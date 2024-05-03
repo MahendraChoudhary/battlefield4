b0ffb4b8e91f138ac604249dfc01967e92c526d7
Mahendra_Choudhary <mahendrajat072003@gmail.com>
2024-05-03 15:05:24 +0530

    fourth commit - generated static build

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,30 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 dotenv.config();
 
 dbConnection = require('./dbConnection');
 const router = require('./routes/router');
 
-const app = express();
 const PORT = process.env.PORT || 4000;
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}))
-
 app.use(express.json());
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    // credentials: true
+}))
+
 app.use('/router', router);
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './build/index.html'));
+})
 
 app.listen(PORT, () => {
         console.clear();
